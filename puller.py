f8af83a8e1c32fa68dfb883639ca02adcd284827
Naveen <33922357+naveen7v@users.noreply.github.com>
2018-04-26 07:47:37 +0000

    Update puller.py

--- puller.py
+++ puller.py
@@ -2,8 +2,6 @@ import requests, os, pandas as pd
 from io import StringIO
 from multiprocessing import Pool
 
-NO_OF_DAYS = input('Enter no. of days to pull:') #google doesnt give more than 15 days of data
-EXCHANGE = 'NSE'
 INTERVAL = '61'  # 61 for 1 minute(60+1) , 601 for 10 minutes(600+1) , 301 for 5 minutes etc(300+1), 
                  # 1 is added to get the timestamp data for parsing
 DOWNLOAD_PATH = '/home/LTP/'
@@ -32,25 +30,28 @@ for stock in stocks:
     stockfile.write('Date,Time,Open,High,Low,Close,Volume\n')
     stockfile.close()
 
-def puller(STOCK, NO_OF_DAYS, EXCHANGE, INTERVAL, WRITE_TO_FILE=True):
-    NO_OF_DAYS = str(NO_OF_DAYS)  # IN CASE ,YOU WANT TO USE THIS FUNCTION IN A ANOTHER SCRIPT
-    EXCHANGE = str(EXCHANGE)
-    INTERVAL = str(INTERVAL)
-    p = requests.get('http://finance.google.com/finance/getprices?q='+STOCK+'&x='+EXCHANGE+'&i='+INTERVAL+'&p='+NO_OF_DAYS+'d&f=d,c,h,l,o,v').text
-    a = pd.read_csv(StringIO(p), skiprows=range(7), names =['date', 'Close', 'High', 'Low', 'Open', 'Volume'])
+def puller(stock, no_of_days, Interval, write_to_file = True):
+    no_of_days = str(no_of_days)
+    Interval = str(Interval)
+    p=requests.get('http://finance.google.com/finance/getprices?q='+stock+'&x=NSE&i='+Interval+'&p='+no_of_days+'d&f=d,c,h,l,o,v').text
+    a=pd.read_csv(StringIO(p),skiprows=range(7),names =['date','Close','High','Low','Open','Volume'])
     
-    a['date']=pd.to_datetime(a.date.str[1:],unit='s').dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
-    a['Date']=a.date.dt.date
-    a['Time']=a.date.dt.time
-    a=a[['Date', 'Time', 'Open', 'High', 'Low', 'Close', 'Volume']]
-
-    if WRITE_TO_FILE:
-      a.to_csv(DOWNLOAD_PATH+stock+'.csv', mode='a', header=False, index=False)
-      print(STOCK)
+    if write_to_file:
+        a['Date'] = pd.to_datetime(a.date.str[1:],unit='s').dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata').dt.strftime('%Y%m%d')
+        a['Time'] = pd.to_datetime(a.date.str[1:],unit='s').dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata').dt.strftime('%H%M%S')
+    
+        a=a[['Date','Time','Open','High','Low','Close','Volume']]
+        a.to_csv('/home/nav/bhavcopy/Intraday_data/2018/'+stock+'/'+stock+'.csv', mode='a', header=False,index=False)
+        print(stock)
     else:
-      return a
+        a['date']=pd.to_datetime(a.date.str[1:],unit='s').dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
+        a['Date']=a.date.dt.date
+        a['Time']=a.date.dt.time
+        a=a[['Date','Time','Open','High','Low','Close','Volume']]
+        return a
      
 if __name__ == '__main__':
-        pool = Pool(processes = 10)
-        for STOCK in stocks:
-            pool.apply_async(func=puller, args=(STOCK, NO_OF_DAYS, EXCHANGE, INTERVAL))
+  NO_OF_DAYS = input('Enter no. of days to pull:') #google doesnt give more than 15 days of data
+  pool = Pool(processes = 10)
+  for STOCK in stocks:
+      pool.apply_async(func=puller, args=(STOCK, NO_OF_DAYS, INTERVAL))
